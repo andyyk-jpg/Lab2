4c1e3a7b5ce4be2be4bea35b092f5450f1bc5063
andyyk <andyy@hello.localdomain>
2025-10-07 21:59:40 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,6 +7,27 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+   if(cnt==M){
+      for(int elem : vec){
+         cout<< elem << " ";
+      }
+      cout<<endl;
+      return;
+   }
+   int biggest_in_vector=0;
+   if(vec.empty()){
+      biggest_in_vector=0;
+   }
+   else{
+      biggest_in_vector=vec.back();
+   }
+
+   for(int i=biggest_in_vector+1;i<=N;i++){
+         vec.push_back(i);
+         func(cnt+1);
+         vec.pop_back();
+      }
+
 
 
 
